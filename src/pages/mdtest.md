8a7d46739371409e0191a7cea3a39b46a8da1b3f
Matt O'Leary <matto2@users.noreply.github.com>
2024-05-16 14:14:14 -0700

    changes to every page

---
+++ src/pages/mdtest.md
@@ -0,0 +1,20 @@
+---
+layout: ../layouts/Base.astro
+title: "Boo"
+---
+
+# Hey
+
+yu suhg ohug ouhgf oaug oauhgv osfuhgv sodufgv sodufhgv siduhfgv sidufhgv sdiufhgv sdiufkhvg sdkfhvgsd fghv
+
+<div class="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+  <div class="bg-red-500 min-h-60">01</div>
+  <div>02</div>
+  <div>03</div>
+  <div>04</div>
+  <div class="bg-green-500 min-h-60">05</div>
+  <div>06</div>
+  <div>07</div>
+  <div>08</div>  
+  <div class="bg-blue-500 min-h-60">09</div>
+</div>
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,14 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+const { withAnimations } = require('animated-tailwindcss');
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = withAnimations({
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['"Montserrat Variable"', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+});
