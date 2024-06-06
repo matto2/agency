ba69d6a2079227e50b50c53b2699fd0f00cb9bc8
Matt O'Leary <matto2@users.noreply.github.com>
2024-06-05 18:25:48 -0700

    fixed lightbox

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import mdx from "@astrojs/mdx";
 
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), icon()]
+  integrations: [tailwind(), icon()]
 });
