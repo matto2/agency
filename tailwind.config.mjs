344e356bab0a4b2a2e40ffb34bf04e45679b038c
Matt O'Leary <matto2@users.noreply.github.com>
2024-08-30 16:57:58 -0700

    css changes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,6 +5,12 @@ module.exports = withAnimations({
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
+      backgroundImage: {
+        'gradient-top-left': 'radial-gradient(circle at top left, #FFD6E8, transparent)',
+        'gradient-top-right': 'radial-gradient(circle at top right, #D6EFFF, transparent)',
+        'gradient-bottom-left': 'radial-gradient(circle at bottom left, #FFECD6, transparent)',
+        'gradient-bottom-right': 'radial-gradient(circle at bottom right, #D6FFD9, transparent)',
+      },
       fontFamily: {
         sans: [
           '-apple-system',
